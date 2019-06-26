00bd39c9f29af0464d376458e095f77456c5137a
fromkut <fromkut@yandex.ru>
2019-06-26 19:44:22 +0300

    1

--- treeselect.js
+++ treeselect.js
@@ -6,9 +6,7 @@ $(function () {
     var urls = {};
 
     init(function (data) {
-
         urls = data.urls;
-
         treeBuilding(data.data, 1, $(ContainerElementTree));
     });
 
@@ -17,16 +15,16 @@ $(function () {
         var dataInit = {
             data: [
                 {
-                    id: 1,
+                    id: (+new Date()),
                     name: "Страна 1",
                     cnt_children: 12
                 },
                 {
-                    id: 2,
+                    id: (+new Date()),
                     name: "Страна 2",
                     cnt_children: 60
                 }, {
-                    id: 3,
+                    id: (+new Date()),
                     name: "Страна 3",
                     cnt_children: 30
                 }
@@ -35,11 +33,11 @@ $(function () {
                 "parents": 'http://localhost/parents/?token=123',
                 "children": 'http://localhost/children/?token=123',
                 'autocomplete': 'http://localhost/ac/?token=123'
-            }
+            },
+
         };
         cb(dataInit);
     }
-
 
     function filter(data, text) {
 
@@ -69,74 +67,9 @@ $(function () {
             sendSearchQuery(query);
         }, e.type === 'input' ? 200 : 0);
     });
-    function sendSearchQuery(query) { }
+    function sendSearchQuery(query) {
 
-    function dataGenerate() {
-        var data = [];
-        var lvl1 = findGetParameter('lvl1') || 6,
-            lvl2 = findGetParameter('lvl2') || 6,
-            lvl3 = findGetParameter('lvl3') || 6;
-
-        $('input[name="lvl1"]').val(findGetParameter('lvl1') || 6);
-        $('input[name="lvl2"]').val(findGetParameter('lvl2') || 6);
-        $('input[name="lvl3"]').val(findGetParameter('lvl3') || 6);
-        var ii = 1;
-        for (var i = 1; i < lvl1; i++) {
-            data.push(itemGenerate(ii, 'Страна'));
-            ii++;
-        }
-
-        for (var p in data) {
-            for (var i = 1; i < lvl2; i++) {
-                data[p].children.push(itemGenerate(ii, 'C' + (data[p].id) + ' Регион'));
-                ii++;
-            }
-        }
-
-        for (var p in data) {
-            for (var p2 in data[p].children) {
-                for (var i = 1; i < lvl3; i++) {
-                    data[p].children[p2].children.push(itemGenerate(ii, 'C' + (data[p].id) + ' Р' + data[p].children[p2].id + ' Город'));
-                    ii++;
-                }
-            }
-        }
-
-        function itemGenerate(id, name) {
-            return {
-                id: id,
-                name: `${name} ${id} `,
-                children: []
-            };
-        }
-
-        function findGetParameter(parameterName) {
-            var result = null,
-                tmp = [];
-            location.search
-                .substr(1)
-                .split("&")
-                .forEach(function (item) {
-                    tmp = item.split("=");
-                    if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
-                });
-            return result;
-        }
-
-        return data;
     }
-
-    // var data = dataGenerate();
-
-    // function requestData(callback) {
-    //     setTimeout(function () {
-    //         callback(data);
-    //     }, 200);
-    // }
-
-    // requestData(function (data) {
-    //     treeBuilding(data);
-    // });
 
     function treeBuilding(data, lvl, container) {
         var threeArr = handler(data, lvl);
@@ -155,7 +88,8 @@ $(function () {
         function handler(arr, lvl) {
             var tpl = [];
             for (var p in arr) {
-                tpl.push(tplItem(arr[p].id, arr[p].name, arr[p].cnt_children, lvl));
+                var ch = arr[p].cnt_children && arr[p].cnt_children.length ? arr[p].cnt_children : 0;
+                tpl.push(tplItem(arr[p].id, arr[p].name, ch, lvl));
             }
             if (tpl.length) {
                 tpl.push('</ul>');
@@ -209,14 +143,17 @@ $(function () {
     $(MainContainer).on('change', '.tree-element-checkbox', function () {
         var item = this;
         var children = $(item).parent().nextAll('ul');
+        $(item).parent().removeClass('children-selected');
         // выбран родитель. проверим есть ли дети
         // если есть, то проставим им выбор + флаг, что выбраны родителем
         if (children.length > 0) {
             $(children).find('input').each(function (i, e) {
+                if (item.checked) {
+                    $(e).parent().removeClass('children-selected');
+                }
                 $(e).prop('checked', item.checked ? true : false);
             });
         }
-
         updThree(item);
     });
 
@@ -239,8 +176,8 @@ $(function () {
                     loadingData.push(id);
                     loadChildren(id, function () {
                         if (item.is(':checked')) {
-                            icon.siblings('ul').find('input').each(function(i, e) {
-                                $(e).prop('checked',true);
+                            icon.siblings('ul').find('input').each(function (i, e) {
+                                $(e).prop('checked', true);
                             });
                         }
                         icon.siblings('ul').show(0);
@@ -279,5 +216,25 @@ $(function () {
         }, 300);
     }
 
+    function loadParent(itemsIds, cb) {
+        // send request: urls.parent + '&ids=' + itemsIds.join(',')
+        setTimeout(function () {
+            var data = [
+                {
+                    1: [123, 321]
+                },
+                {
+                    2: [123, 321]
+                },
+                {
+                    3: [123, 321]
+                }
+            ];
+            var container = $('[data-id="' + parent_id + '"]').parent().parent();
+            var lvl = (+$('[data-id="' + parent_id + '"]').data('lvl')) + 1;
+            treeBuilding(data, lvl, container);
+            cb();
+        }, 300);
+    }
 
 });
