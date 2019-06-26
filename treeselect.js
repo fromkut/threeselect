f2ed2d6effbafd7bce8a83c63787a7cb6570011c
fromkut <fromkut@yandex.ru>
2019-06-26 18:28:41 +0300

    1

--- treeselect.js
+++ treeselect.js
@@ -1,14 +1,15 @@
 $(function () {
 
-    var containerElementTree = $('.tree-elements');
+    const MainContainer = '#treeselect',
+        ContainerElementTree = '.tree-elements';
+
+    var urls = {};
 
     init(function (data) {
 
-        var urls = data.urls,
-            data = data.data;
+        urls = data.urls;
 
-        treeBuilding(data);
-
+        treeBuilding(data.data, 1, $(ContainerElementTree));
     });
 
     function init(cb) {
@@ -24,7 +25,7 @@ $(function () {
                     id: 2,
                     name: "Страна 2",
                     cnt_children: 60
-                },{
+                }, {
                     id: 3,
                     name: "Страна 3",
                     cnt_children: 30
@@ -137,28 +138,24 @@ $(function () {
     //     treeBuilding(data);
     // });
 
-    function treeBuilding(data) {
-        var threeArr = handler(data, 1);
-        containerElementTree.html(threeArr);
-        setHandler();
+    function treeBuilding(data, lvl, container) {
+        var threeArr = handler(data, lvl);
+        container.append(threeArr);
 
         function tplItem(id, name, children, lvl) {
             return `<li>
                         ${children ? '<span class="toggle splus"></span>' : ''}
-                        <input type="checkbox" class="tree-element" data-lvl="${lvl}" name="item_${id}"><span>${name}</span>
-                        ${children}
+                        <span class="tree-element">
+                            <input type="checkbox" class="tree-element-checkbox" data-lvl="${lvl}" data-cnt-children="${children}" data-id="${id}" name="i_${id}">
+                        </span
+                        <span>${name}</span>
                     </li>`;
         }
 
         function handler(arr, lvl) {
             var tpl = [];
             for (var p in arr) {
-                var children = '';
-                if (arr[p].children && arr[p].children.length) {
-                    var newLvl = lvl + 1;
-                    children = handler(arr[p].children, newLvl);
-                }
-                tpl.push(tplItem(arr[p].id, arr[p].name, children, lvl));
+                tpl.push(tplItem(arr[p].id, arr[p].name, arr[p].cnt_children, lvl));
             }
             if (tpl.length) {
                 tpl.push('</ul>');
@@ -170,20 +167,37 @@ $(function () {
     }
 
     function updThree(item) {
+
         var lvl = $(item).data('lvl');
         var items = $(item).closest('ul').find(`input[data-lvl="${$(item).data('lvl')}"]`);
-        var isAllCheck = true;
+        var parent = $(item).parent().parent().parent().parent().find(`input[data-lvl="${lvl - 1}"]`);
+
+        var countCheckedItems = 0;
+        var countCheckedChildrenItems = 0;
+
         items.each(function (i, e) {
-            if (!e.checked) {
-                isAllCheck = false;
-                return;
+            if (e.checked) {
+                countCheckedItems++;
+            }
+            if ($(e).parent().hasClass('children-selected')) {
+                countCheckedChildrenItems++;
             }
         });
-        var parent = $(item).parent().parent().parent().find(`input[data-lvl="${lvl - 1}"]`);
-        if (isAllCheck) {
+
+        if (countCheckedItems === items.length) {
             parent.prop('checked', true);
+            parent.parent().removeClass('children-selected');
         } else {
             parent.prop('checked', false);
+            if (countCheckedItems > 0) {
+                parent.parent().addClass('children-selected');
+            } else {
+                if (countCheckedChildrenItems) {
+                    parent.parent().addClass('children-selected');
+                } else {
+                    parent.parent().removeClass('children-selected');
+                }
+            }
         }
 
         if (parent.length > 0) {
@@ -191,28 +205,79 @@ $(function () {
         }
     }
 
-    function setHandler() {
-        $(".tree-element").change(function () {
 
-            var item = this;
-            var children = $(item).nextAll('ul');
-            // выбран родитель. проверим есть ли дети
-            // если есть, то проставим им выбор + флаг, что выбраны родителем
-            if (children.length > 0) {
-                $(children).find('input').each(function (i, e) {
-                    $(e).prop('checked', item.checked ? true : false);
-                });
-            }
-
-            updThree(item);
-        });
-
-        $(".toggle").click(function () {
-            var item = $(this);
-            item.nextAll('ul').toggle(0, function () {
-                item.toggleClass('splus');
+    $(MainContainer).on('change', '.tree-element-checkbox', function () {
+        var item = this;
+        var children = $(item).parent().nextAll('ul');
+        // выбран родитель. проверим есть ли дети
+        // если есть, то проставим им выбор + флаг, что выбраны родителем
+        if (children.length > 0) {
+            $(children).find('input').each(function (i, e) {
+                $(e).prop('checked', item.checked ? true : false);
             });
-        });
+        }
+
+        updThree(item);
+    });
+
+    // чтобы во время загрузки не обрабатывать повторные клики
+    var loadingData = [];
+
+    $(MainContainer).on('click', '.toggle', function () {
+        var icon = $(this);
+        var children = icon.siblings('ul');
+        // если уже загружали детей
+        if (children.length > 0) {
+            children.toggle(0, function () {
+                icon.toggleClass('splus');
+            });
+        } else {
+            var item = $(this).siblings('.tree-element').children('input');
+            if (item.data('cnt-children')) {
+                var id = item.data('id');
+                if (loadingData.indexOf(id) === -1) {
+                    loadingData.push(id);
+                    loadChildren(id, function () {
+                        if (item.is(':checked')) {
+                            icon.siblings('ul').find('input').each(function(i, e) {
+                                $(e).prop('checked',true);
+                            });
+                        }
+                        icon.siblings('ul').show(0);
+                        icon.toggleClass('splus');
+                        var indexID = loadingData.indexOf(id);
+                        delete loadingData[indexID];
+                    });
+                }
+            }
+        }
+    });
+
+    function loadChildren(parent_id, cb) {
+        // send request: urls.children + '&parent=' + parent_id
+        setTimeout(function () {
+            var data = [
+                {
+                    id: (+ new Date()),
+                    name: "Регион 1",
+                    cnt_children: 12
+                },
+                {
+                    id: (+ new Date()),
+                    name: "Регион 2",
+                    cnt_children: 60
+                }, {
+                    id: (+ new Date()),
+                    name: "Регион 3",
+                    cnt_children: 30
+                }
+            ];
+            var container = $('[data-id="' + parent_id + '"]').parent().parent();
+            var lvl = (+$('[data-id="' + parent_id + '"]').data('lvl')) + 1;
+            treeBuilding(data, lvl, container);
+            cb();
+        }, 300);
     }
+
 
 });
